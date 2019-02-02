3278615d96946f2dc73b2fe4af02aae7eef07dfc
Bcecil <blakemadness@gmail.com>
2019-02-01 17:01:54 -0800

    completed HACK implementation of fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,6 +1,4 @@
 // This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
 // Runs an infinite loop that listens to the keyboard input.
@@ -11,4 +9,72 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+//initialize current pixel to point at base of screen memory
+@SCREEN
+D=A
+@currentpixel
+M=D
+//initalize constant to address of last screen pixel
+@KBD
+D=A
+@lastpixel
+M=D
+//infinite loop that listens for keyboard input
+(INFLOOP)
+
+// check keyboard if key pressed jump to make pixel 
+
+@KBD
+D=M
+@MAKEPIXEL
+D;JNE
+
+// else jump to delete pixel
+
+@DELETEPIXEL
+0;JMP
+
+
+(MAKEPIXEL)
+// checks to see if index is in valid range by doing lastpixel - currentpixel
+@currentpixel
+D=M
+@lastpixel
+D=D-M
+
+// if d == 0 we have an invalid index and are done
+@INFLOOP
+D;JEQ
+
+// else get the address of the current pixel, set to black, then increment
+@currentpixel
+A=M
+M=-1
+@currentpixel
+M=M+1
+
+// check to see if key is still being pressed
+@INFLOOP
+0;JMP
+
+(DELETEPIXEL)
+// check to see if index is valid by doing currentpixel - SCREEN
+@SCREEN
+D=A
+@currentpixel
+D=M-D
+
+// if d < 0 we have and invalid index and are done
+@INFLOOP
+D;JLT
+
+// else get the address of the current pixel, set to white, then increment
+@currentpixel
+A=M
+M=0
+@currentpixel
+M=M-1
+
+// check to see if no key is being pressed
+@INFLOOP
+0;JMP
