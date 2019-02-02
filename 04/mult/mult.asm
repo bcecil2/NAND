355f6195431524f701720e1de1a29575dc648282
Bcecil <blakemadness@gmail.com>
2019-02-01 17:11:12 -0800

    completed HACK implementation of mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,54 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+(SETUP)
+
+@product //stores the final result
+M=0
+
+@R2 //sets R2 to zero 
+M=0
+
+
+// storing R0 into variable a to be used in loop
+@R0 
+D=M
+@a 
+M=D
+
+// storing R1 into variable i to be used as loop counter
+@R1
+D=M
+@i
+M=D
+
+(LOOP)
+
+// if i == 0 jump to end
+@i
+D=M
+@END
+D;JEQ
+
+// product += a
+@a  
+D=M
+@product 
+M=D+M 
+
+// i = i - 1
+@i
+M=M-1 
+@LOOP
+0;JMP
+
+(END)
+//store the result of product in R2
+@product
+D=M
+@R2
+M=D
+
+(THEREALEND)
+@THEREALEND
+0;JMP
