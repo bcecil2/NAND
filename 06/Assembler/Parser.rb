f4b30fb38361a46c057585d6ac2b28645ffdca9b
Bcecil <blakemadness@gmail.com>
2019-02-09 21:59:55 -0800

    completed symbol-less parser module

--- 06/Assembler/Parser.rb
+++ 06/Assembler/Parser.rb
@@ -6,6 +6,7 @@ class Parser
 	attr_reader :file_size
 	attr_reader :assembly_file
 	attr_reader :currentcommand
+	attr_reader :currentlinenumber
 
 	def initialize()
 		name = get_filename
@@ -16,37 +17,81 @@ class Parser
 	end
 
 	def has_more_commands()
-		@currentlinenumber < @linecount
+		@currentlinenumber + 1 < @linecount
 	end
 
 	def advance()
 		if has_more_commands
-			@currentcommand = @assembly_file.gets.chomp.gsub(/\s+/, '')
-			@currentlinenumber += 1
+			@currentcommand = @assembly_file.gets.chomp.gsub(/\s+/, '') # gets line, chomp removes newline characters, gsub hack shit to remove whitespace
+			@currentlinenumber = $. - 1 # evil magic variable that tells you the number of last line read
 		end
 	end
 
-	def command_type()
+	def command_type(command)
 		type = ""
-		unless @currentcommand.empty?
-			if @currentcommand.match(/^[\/]{2}/) # matches // placed at beginning of line
+		unless command.empty?
+			if command.match(/^[\/]{2}/) # matches // placed at beginning of line
 				type = "COMMENT"
-			elsif @currentcommand.match(/^[@]\d/) # matches @ placed at beginning of line followed by any digit
+			elsif command.match(/^[@]\d/) # matches @ placed at beginning of line followed by any digit
 				type = "A"
-			elsif @currentcommand.match(/([=]|[;])/) # mathches any line containg = or ; placed at beginning of line 
+			elsif command.match(/([=]|[;])/) # mathches any line containg = or ; placed at beginning of line 
 				type = "C"
 			end
 		end
 	end
+
+	def symbol(command)
+		symbol_string =""
+		if command_type(command) == "A"
+			symbol_string = command[1..command.size] # slices anything past @
+		end
+	end
+
+	def dest(command)
+		dest_string = ""
+		if command_type(command) == "C"
+			if command.match(/[=]/)
+				dest_string = command[/^[AMD]/] # returns the substring of any matches to the left of =
+			end
+		end
+	end
+
+	def comp(command)
+		comp_string = ""
+		if command_type(command) == "C"
+			if command.match(/[=]/)
+				computation_array = command.split("=") # splits string on either side of the equals sign
+				comp_string = computation_array[1]
+			end
+		end
+	end
+
+	def jump(command)
+		jump_string = ""
+		if command_type(command) == "C"
+			if command.match(/[;]/)
+				computation_array = command.split(";") # splits string on either side of the ;
+				comp_string = computation_array[1]
+			end
+		end
+	end
+
 end
 
 boi = Parser.new()
-
+#while boi.has_more_commands
+#		boi.advance
+#		unless boi.currentcommand.empty?
+#			puts " current command is #{boi.currentcommand} and is of type #{boi.command_type(boi.currentcommand)}"	
+#			if boi.command_type(boi.currentcommand) == "A"
+#				 #puts boi.symbol(boi.currentcommand)	
+#			elsif boi.command_type(boi.currentcommand) == "C"
+#				 puts boi.comp(boi.currentcommand)
+#			end
+#				
+#		end
+#end
 while boi.has_more_commands
-	unless boi.currentcommand.empty?
-		boi.advance
-		puts " current command is #{boi.currentcommand} and is of type #{boi.command_type}"
-	end
-	boi.advance		
+	boi.advance
+	puts "current line #{boi.currentlinenumber} current command #{boi.currentcommand} "	
 end
-
