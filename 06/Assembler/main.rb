f990174f681a1cc3c3a068cfe528eb0b9b0cefed
Bcecil <blakemadness@gmail.com>
2019-02-10 19:00:42 -0800

    refactored main to implement assembler interface

--- 06/Assembler/main.rb
+++ 06/Assembler/main.rb
@@ -1,12 +1,7 @@
-load "CodeGenerator.rb"
-load "Parser.rb"
+load "Assembler.rb"
 
 def valid_args
 	ARGV.size == 1 && File.extname(ARGV[0]) == ".asm" && File.exist?(ARGV[0])
-end
-
-def get_filename
-	file_name = ARGV[0]
 end
 
 if  valid_args
@@ -16,28 +11,10 @@ if  valid_args
 	hack_file = "#{asm_path}\\#{incoming_filename}.hack"
 	assembly_file = File.open("#{asm_filename}", "r")
 	file_to_fill = File.open("#{hack_file}", "w")
-	code_parser = Parser.new
-	code_generator = CodeGenerator.new
-	while code_parser.has_more_commands
-		code_parser.advance
-		unless code_parser.currentcommand.empty?
-			if code_parser.command_type() == "A"
-				x = code_parser.symbol().to_i.to_s(2).rjust(16, "0")
-				file_to_fill << x << "\n"
-			elsif code_parser.command_type() == "C"
-				y = "111"
-				y << code_generator.comp(code_parser.comp()) << code_generator.dest(code_parser.dest()) << code_generator.jump(code_parser.jump()) << "\n"
-				file_to_fill << y   
-			end	
-		end
-	end
+	assembler = Assembler.new(ARGV[0])
+	assembly_file.each do |line|
+		assembler.assemble_line(line, file_to_fill)
+	end	
 else
 	puts "call to main with invalid arguments"
 end
-
-
-
-
-
-
-
