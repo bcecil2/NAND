a7fbac307006ab61dddc6bcb9a7c5760ae5ca1a4
Bcecil <blakemadness@gmail.com>
2019-02-11 15:03:55 -0800

    added symbol table and first pass method to assembler

--- 06/Assembler/Assembler.rb
+++ 06/Assembler/Assembler.rb
@@ -1,13 +1,29 @@
 load "CodeGenerator.rb"
 load "Parser.rb"
+load "SymbolTable.rb"
 
 
 class Assembler
 
+	attr_reader :Parser
+	attr_reader :symbol_table
+
 	def initialize(file)
 		@CodeGenerator = CodeGenerator.new
 		@Parser = Parser.new(file)
+		@symbol_table = SymbolTable.new
+		@ROM_address = 0
 	end
+
+	def first_pass(incoming_line)
+		@Parser.advance(incoming_line)
+		if @Parser.command_type == "A" || @Parser.command_type == "C"
+			@ROM_address += 1
+		elsif @Parser.command_type == "L"
+			@symbol_table.add_entry(@Parser.currentcommand, @ROM_address)
+		end
+	end
+		 	 
 
 	def assemble_line(incoming_line, file_to_write)
 		@Parser.advance(incoming_line)
