0a2377330c29855a6f0864b0d773ea06077849ac
Bcecil <blakemadness@gmail.com>
2019-02-11 18:44:51 -0800

    added second pass method to assembler

--- 06/Assembler/Assembler.rb
+++ 06/Assembler/Assembler.rb
@@ -13,29 +13,47 @@ class Assembler
 		@Parser = Parser.new(file)
 		@symbol_table = SymbolTable.new
 		@ROM_address = 0
+		@variable_count = 0
 	end
 
 	def first_pass(incoming_line)
 		@Parser.advance(incoming_line)
-		if @Parser.command_type == "A" || @Parser.command_type == "C"
+		if @Parser.command_type == "A" || @Parser.command_type == "C" # if we have an A or C command increment the ROM line counter
 			@ROM_address += 1
-		elsif @Parser.command_type == "L"
-			@symbol_table.add_entry(@Parser.currentcommand, @ROM_address)
+		elsif @Parser.command_type == "L" # if we have a label get its name and create an entry in the symbol table with its corresponding address
+			@symbol_table.add_entry(@Parser.currentcommand.gsub(/^[(]|[)]/, ""), @ROM_address.to_s(2).rjust(16, "0"))
 		end
 	end
-		 	 
 
-	def assemble_line(incoming_line, file_to_write)
-		@Parser.advance(incoming_line)
-		if @Parser.command_type == "A"
-			a_command = @Parser.symbol().to_i.to_s(2).rjust(16, "0")
-			file_to_write << a_command << "\n"
+	def second_pass(incoming_line)
+		@Parser.currentlinenumber = 0 # reset the Parser for the second pass
+		@Parser.currentcommand = 0
+		@Parser.advance(incoming_line) # reads in incoming line
+		@Parser.currentcommand
+		if @Parser.command_type == "A" # if its an A command
+			variable_name = @Parser.symbol # grab whats to the right of the @
+			#puts variable_name 
+			unless @Parser.currentcommand.match(/^@[^a-zA-Z]/) # if its a variable
+				if @symbol_table.symbols.key?(variable_name) # if the variable already exists
+					#puts "getting key for #{variable_name}"
+					variable_name = @symbol_table.symbols[variable_name] # get the key
+					#puts "key for #{variable_name}"
+					@Parser.currentcommand = variable_name # translate it to binary
+
+				else # new variable translate to binary ram address starting at 16
+					@Parser.currentcommand = @symbol_table.add_entry(variable_name, (16 + @variable_count).to_s(2).rjust(16, "0"))
+					@variable_count += 1
+				end
+			else # just a constant that can be converted
+				@Parser.currentcommand = variable_name.to_i.to_s(2).rjust(16, "0")
+			end
 		elsif @Parser.command_type() == "C"
-				c_command = "111"
-				c_command << @CodeGenerator.comp(@Parser.comp()) << @CodeGenerator.dest(@Parser.dest()) << @CodeGenerator.jump(@Parser.jump()) 
-				file_to_write << c_command << "\n" 
-		end	
+			    c_command = "111"
+				@Parser.currentcommand = c_command << @CodeGenerator.comp(@Parser.comp()) << @CodeGenerator.dest(@Parser.dest()) << @CodeGenerator.jump(@Parser.jump())
+		end
+		return @Parser.currentcommand
 	end
+		 	 
 end
 
 
