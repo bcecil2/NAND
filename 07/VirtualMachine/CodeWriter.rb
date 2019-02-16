01bf843e9dd02374ead63ae868386a0ab73acf39
Bcecil <blakemadness@gmail.com>
2019-02-16 15:01:15 -0800

    refactored code writer to use hash table for write_arithmetic

--- 07/VirtualMachine/CodeWriter.rb
+++ 07/VirtualMachine/CodeWriter.rb
@@ -4,31 +4,29 @@ class CodeWriter
 	def initialize(file_to_write)
 		@asm_file = File.open(file_to_write, "w")
 		@label_count = 1
+		@binary_operator_table = {"add" => "M=M+D\n",
+		 					 	  "sub" => "M=M-D\n",
+		 				     	  "and" => "M=M&D\n",
+		 				          "or" => "M=M|D\n"}
+		 				     
+		@unary_operator_table = {"neg" => "M=-M\n","not" => "M=!M\n"}
 	end
 
 	def write_arithmetic(command , line)
 		@asm_file << "//#{line}\n"
-		if command == "add"
-			@asm_file << "#{adjust_sp}M=M+D\n@SP\nM=M+1\n"
-		elsif command == "sub"
-			@asm_file << "#{adjust_sp}M=M-D\n@SP\nM=M+1\n"
-		elsif command == "neg"
-			@asm_file << "@SP\nM=M-1\n@SP\nA=M\nM=-M\n@SP\nM=M+1\n"
-		elsif command == "eq"
-			@asm_file << "#{adjust_sp}D=M-D\n@EQUAL#{@label_count}\nD;JEQ\n@NOTEQUAL#{@label_count}\n0;JMP\n(EQUAL#{@label_count})\n@SP\nA=M\nM=-1\n@CHANGESP#{@label_count}\n0;JMP\n(NOTEQUAL#{@label_count})\n@SP\nA=M\nM=0\n(CHANGESP#{@label_count})\n@SP\nM=M+1\n"			
+		if command == "eq"
+			@asm_file << "#{adjust_sp_binary}D=M-D\n@EQUAL#{@label_count}\nD;JEQ\n@NOTEQUAL#{@label_count}\n0;JMP\n(EQUAL#{@label_count})\n@SP\nA=M\nM=-1\n@CHANGESP#{@label_count}\n0;JMP\n(NOTEQUAL#{@label_count})\n@SP\nA=M\nM=0\n(CHANGESP#{@label_count})\n#{increment_sp}"			
 			@label_count += 1
 		elsif command == "lt"
-			@asm_file << "#{adjust_sp}D=M-D\n@LESSTHAN#{@label_count}\nD;JLT\n@GREATERTHAN#{@label_count}\n0;JMP\n(LESSTHAN#{@label_count})\n@SP\nA=M\nM=-1\n@CHANGESP#{@label_count}\n0;JMP\n(GREATERTHAN#{@label_count})\n@SP\nA=M\nM=0\n@CHANGESP#{@label_count}\n0;JMP\n(CHANGESP#{@label_count})\n@SP\nM=M+1\n"
+			@asm_file << "#{adjust_sp_binary}D=M-D\n@LESSTHAN#{@label_count}\nD;JLT\n@GREATERTHAN#{@label_count}\n0;JMP\n(LESSTHAN#{@label_count})\n@SP\nA=M\nM=-1\n@CHANGESP#{@label_count}\n0;JMP\n(GREATERTHAN#{@label_count})\n@SP\nA=M\nM=0\n@CHANGESP#{@label_count}\n0;JMP\n(CHANGESP#{@label_count})\n#{increment_sp}"
 			@label_count += 1
 		elsif command == "gt"
-			@asm_file << "#{adjust_sp}D=M-D\n@LESS#{@label_count}\nD;JLT\n@GREATER#{@label_count}\nD;JGT\n@LESS#{@label_count}\nD;JEQ\n(LESS#{@label_count})\n@SP\nA=M\nM=0\n@CHANGESP#{@label_count}\n0;JMP\n(GREATER#{@label_count})\n@SP\nA=M\nM=-1\n@CHANGESP#{@label_count}\n0;JMP\n(CHANGESP#{@label_count})\n@SP\nM=M+1\n"
+			@asm_file << "#{adjust_sp_binary}D=M-D\n@LESS#{@label_count}\nD;JLT\n@GREATER#{@label_count}\nD;JGT\n@LESS#{@label_count}\nD;JEQ\n(LESS#{@label_count})\n@SP\nA=M\nM=0\n@CHANGESP#{@label_count}\n0;JMP\n(GREATER#{@label_count})\n@SP\nA=M\nM=-1\n@CHANGESP#{@label_count}\n0;JMP\n(CHANGESP#{@label_count})\n#{increment_sp}"
 			@label_count += 1
-		elsif command == "and"
-			@asm_file << "#{adjust_sp}M=M&D\n@SP\nM=M+1\n"
-		elsif command == "or"
-			@asm_file << "#{adjust_sp}M=M|D\n@SP\nM=M+1\n"
-		elsif command == "not"
-			@asm_file << "@SP\nM=M-1\n@SP\nA=M\nM=!M\n@SP\nM=M+1\n"	
+		elsif @binary_operator_table.has_key? command
+			@asm_file << "#{adjust_sp_binary}" << @binary_operator_table[command] << "#{increment_sp}"
+		elsif @unary_operator_table.has_key? command
+			@asm_file << "#{adjust_sp_unary}" << @unary_operator_table[command] << "#{increment_sp}"	
 		end
 
 	end
@@ -36,7 +34,7 @@ class CodeWriter
 	def write_push_pop(command, segment, index, line)
 		@asm_file << "//#{line}\n"
 		if command == "C_PUSH"
-			@asm_file << "@#{index}\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"
+			@asm_file << "@#{index}\nD=A\n@SP\nA=M\nM=D\n#{increment_sp}"
 	    end
 	end
 
@@ -44,9 +42,16 @@ class CodeWriter
 		@asm_file << "(END)\n@END\n0;JMP\n"
 	end
 
-	def adjust_sp()
+	def adjust_sp_binary()
 		"@SP\nM=M-1\n@SP\nA=M\nD=M\n@SP\nM=M-1\nA=M\n"
 	end
 
+	def adjust_sp_unary()
+		"@SP\nM=M-1\n@SP\nA=M\n"
+	end
+
+	def increment_sp()
+		"@SP\nM=M+1\n"
+	end
 
 end
