56a3c46650074a8437013ad22e3b0efdb8e651d8
Bcecil <blakemadness@gmail.com>
2019-02-17 16:54:24 -0800

    changed indention

--- 07/VirtualMachine/VMParser.rb
+++ 07/VirtualMachine/VMParser.rb
@@ -1,73 +1,70 @@
 require_relative "CodeWriter.rb"
 
 class Parser
-	attr_reader :current_line
+  attr_reader :current_line
   
-	def initialize(in_file, out_file)
-		@current_line = ""
-		@vm_file = in_file.readlines
-		@code_writer = CodeWriter.new(out_file)	
-	end
+  def initialize(in_file, out_file)
+    @current_line = ""
+    @vm_file = in_file.readlines
+    @code_writer = CodeWriter.new(out_file) 
+  end
 
-# change this so we dont have to deal with spaces again
-	def clean_line(incoming_line)
-		@current_line = incoming_line.chomp.gsub(" ", '')
-		if @current_line.match(/[\/]{2}/) # checks for inline comment 
-				@current_line = @current_line.split("//") 
-				@current_line = @current_line[0]
-		end
-	end
+  def clean_line(incoming_line)
+    @current_line = incoming_line.chomp.gsub(" ", '')
+    if @current_line.match(/[\/]{2}/) # checks for inline comment 
+        @current_line = @current_line.split("//") 
+        @current_line = @current_line[0]
+    end
+  end
 
-	def command_type()
-		if @current_line.include?("push")
-		  command_type = "C_PUSH"
-		elsif @current_line.include?("pop")
-			command_type = "C_POP"
-		elsif @current_line.match(/\badd\b|\bsub\b|\bneg\b/)
-		  command_type = "C_ARITHMETIC"
-		elsif @current_line.match(/\beq\b|\blt\b|\bgt\b/)
-			command_type = "C_ARITHMETIC"
-		elsif @current_line.match(/\bor\b|\bnot\b|\band\b/)
-			command_type = "C_ARITHMETIC"		
-		end
+  def command_type()
+    if @current_line.include?("push")
+      command_type = "C_PUSH"
+    elsif @current_line.include?("pop")
+      command_type = "C_POP"
+    elsif @current_line.match(/\badd\b|\bsub\b|\bneg\b/)
+      command_type = "C_ARITHMETIC"
+    elsif @current_line.match(/\beq\b|\blt\b|\bgt\b/)
+      command_type = "C_ARITHMETIC"
+    elsif @current_line.match(/\bor\b|\bnot\b|\band\b/)
+      command_type = "C_ARITHMETIC"   
+    end
 
-	end
+  end
 
-	def arg1()
-		if command_type == "C_PUSH"
-			arg1 = @current_line.split("push")[1].split(/[0-9]/)
-		elsif command_type == "C_POP"
-			arg1 = @current_line.split("pop")[1].split(/[0-9]/)
-		elsif command_type == "C_ARITHMETIC" #|| command_type == "C_EQUAL" || command_type == "C_BOOL"
-			arg1 = @current_line
-		end
-	end
+  def arg1()
+    if command_type == "C_PUSH"
+      arg1 = @current_line.split("push")[1].split(/[0-9]/)[0]
+    elsif command_type == "C_POP"
+      arg1 = @current_line.split("pop")[1].split(/[0-9]/)[0]
+    elsif command_type == "C_ARITHMETIC" 
+      arg1 = @current_line
+    end
+  end
 
-	def arg2()
-		if command_type == "C_PUSH"
-			arg2 = @current_line.gsub(/[a-z]/, "")
-		elsif command_type == "C_POP"
-			arg2 = @current_line.gsub(/[a-z]/, "")
-		end
-		arg2.to_i
-	end
-
+  def arg2()
+    if command_type == "C_PUSH"
+      arg2 = @current_line.gsub(/[a-z]/, "")
+    elsif command_type == "C_POP"
+      arg2 = @current_line.gsub(/[a-z]/, "")
+    end
+    arg2.to_i
+  end
 
 
-	def parse()
 
-		@vm_file.each do |line|
-			clean_line(line)
-			unless @current_line.empty?
-				if command_type == "C_PUSH"
-					@code_writer.write_push_pop(command_type, arg1, arg2, line)
-				elsif command_type == "C_ARITHMETIC" #|| command_type == "C_EQUAL" || command_type == "C_BOOL"
-					@code_writer.write_arithmetic(arg1, line)
-				end
-			end
-		end
-		@code_writer.add_end
-	end
+  def parse()
 
-
+    @vm_file.each do |line|
+      clean_line(line)
+      unless @current_line.empty?
+        if command_type == "C_PUSH" || command_type == "C_POP"
+          @code_writer.write_push_pop(command_type, arg1, arg2, line)
+        elsif command_type == "C_ARITHMETIC" 
+          @code_writer.write_arithmetic(arg1, line)
+        end
+      end
+    end
+    @code_writer.add_end
+  end
 end
